20bbd1a1db4fa84171b812761aa1c6061cb2ff92
cy3erdr4g0n <imrg10400@gmail.com>
2024-06-16 14:50:35 +0100

    configuring controller for the project

---
+++ controllers/auth.controllers.js
@@ -0,0 +1,56 @@
+const { errorHandler } = require("../utils/errorHandler")
+module.exports.sigup =(req, res)=>{
+    try {
+        
+    } catch (error) {
+        errorHandler(error, res)
+    }
+}
+
+module.exports.ActivateUser =(req,res)=>{
+try {
+    
+} catch (error) {
+    
+}
+}
+
+module.exports.Otp =(req,res)=>{
+try {
+    
+} catch (error) {
+    
+}
+}
+
+module.exports.recoverAccount = (req,res)=>{
+try {
+    
+} catch (error) {
+    
+}
+}
+
+module.exports.changePassword = (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports.resetPassword = (req,res)=>{
+try {
+    
+} catch (error) {
+    
+}
+}
+
+module.exports.login = (req,res)=>{
+try {
+    
+} catch (error) {
+    
+}
+}
